2fa0318fb9cc5cbc3e8bcd6a47c3cfe581a00ee4
Kaito Udagawa <umireon@gmail.com>
2022-04-19 22:31:16 +0900

    main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"net/http"
+	"os"
+
+	"github.com/lucas-clemente/quic-go/http3"
+)
+
+func generateTLSCert() {
+	key, _ := rsa.GenerateKey(rand.Reader, 1024)
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, _ := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	keyOut, _ := os.Create("key.pem")
+	defer keyOut.Close()
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certOut, _ := os.Create("cert.pem")
+	defer certOut.Close()
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+}
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func main() {
+	generateTLSCert()
+	http3.ListenAndServe("localhost:4242", "cert.pem", "key.pem", http.HandlerFunc(hello))
+}
